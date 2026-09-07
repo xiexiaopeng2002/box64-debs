bed50b0c5edba491680219fd454d4543c53fe265
pengpeng <77517276+xiexiaopeng2002@users.noreply.github.com>
2026-09-07 11:11:31 +0800

    Update create-deb.sh

--- create-deb.sh
+++ create-deb.sh
@@ -56,7 +56,7 @@ for target in ${targets[@]}; do
     cmake .. -DBAD_SIGNAL=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc-16 -DARM_DYNAREC=ON -DBOX32=1 -DBOX32_BINFMT=1 || error "Failed to run cmake."
   else
     #cmake .. -D$target=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc-16 -DARM_DYNAREC=ON -DBOX32=1 -DBOX32_BINFMT=1 || error "Failed to run cmake."
-    cmake .. -DCMAKE_C_FLAGS="-march=armv8-a+crc+simd+crypto -mtune=cortex-a53" -DCMAKE_CXX_FLAGS="-march=armv8-a+crc+simd+crypto -mtune=cortex-a53" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc-16 -DARM_DYNAREC=ON -DBOX32=1 -DBOX32_BINFMT=1 -DSAVE_MEM=ON || error "Failed to run cmake."
+    cmake .. -DCMAKE_C_FLAGS="-mcpu=cortex-a53+crc+crypto -O3" -DCMAKE_CXX_FLAGS="-mcpu=cortex-a53+crc+crypto -O3" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc-16 -DARM_DYNAREC=ON -DBOX32=1 -DBOX32_BINFMT=1 -DSAVE_MEM=ON || error "Failed to run cmake."
   fi
   make -j16 || error "Failed to run make."
 
